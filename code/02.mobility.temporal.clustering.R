1d3a7b0633a09e2b3b019914f9a5023dcfbf71a9
Won Do Lee <wondo.lee@krila.re.kr>
2023-03-28 15:08:28 +0900

    Update

--- code/02.mobility.temporal.clustering.R
+++ code/02.mobility.temporal.clustering.R
@@ -1,9 +1,15 @@
 ##Basic parameter for workspace
 Sys.setlocale(locale="English_United Kingdom")
-setwd("d:/OneDrive - Nexus365/WORK/Document/RESEARCH/COVID19/09_IJGS SI/FINAL_CODE/")
+setwd("d:/WORKSPACE/GIT/covid19-eng-lockdown/process/")
+data.path<-"d:/WORKSPACE/GIT/covid19-eng-lockdown/data/"
+tbl.mob.per<-readRDS(paste0(data.path,"input.mob.data.rda"))
 
-tbl.mob.per<-readRDS("input.mob.data.rda")
+require(future)
+plan(multisession)
+availableCores() #20
+plan(multisession, workers = 12)
 
+require(dtwclust)
 ##Should be excluding the Isle of Sicilly
 dyw.input.mob<-as.data.frame(tbl.mob.per)
 dyw.input.mob<-subset(dyw.input.mob,date>="2020-03-23" & date<"2020-05-12") #23 March 2020 to 11 May 2020 
@@ -50,6 +56,10 @@ require(dplyr)
 h04.per<-as.data.frame(cutree(best.per, k=4L))
 h04.per <- tibble::rownames_to_column(h04.per, "LTLA19CD")
 colnames(h04.per)<- c("LTLA19CD","CLU")
+h04.per$CLU<-as.factor(h04.per$CLU)
+levels(h04.per$CLU)<-c("G4","G3","G1","G2")
+h04.per$CLU<-factor(h04.per$CLU,
+                    levels=c("G1","G2","G3","G4"))
 write.csv(h04.per,"h04.per.dtw.csv")
 saveRDS(h04.per,"mobility.temporal.cluster.rda")
 
@@ -62,57 +72,85 @@ require(ggrepel)
 require(scales)
 require(rgdal)
 require(ggspatial)
-require(extrafont)
-loadfonts(device = "win")
+require(showtext)
+font_add_google("Noto Sans KR", "notosanskr")
+showtext_auto()
 
-ltla<-st_read("D:/OneDrive - Nexus365/WORK/Document/RESEARCH/COVID19/09_IJGS SI/DATA_REVISION/PROCESS/en.ltla.shp")
-eng.la<-st_read("D:/WORKSPACE/COVID19/REVISE_LAST/ENGLAND_REGIONS.shp")
+ltla<-st_read(paste0(data.path,"en.ltla.shp"))
+eng.la<-st_read(paste0(data.path,"ENGLAND_REGIONS.shp"))
 eng.la.coord <- sf::st_point_on_surface(eng.la)
 la.coords <- as.data.frame(sf::st_coordinates(eng.la.coord))
 la.coords$NAME <- eng.la.coord$RGN11NM
 
 ltla<-left_join(ltla,h04.per,by=c("LTLA19CD"))
-ltla$CLU<-as.factor(ltla$CLU)
 ltla<-na.omit(ltla)
-saveRDS(ltla,"ltla.h04.per.dtw.rda")
+st_write(ltla,"ltla.h04.per.dtw.geojson")
 
-map.dtw.h04<-ggplot(ltla)+geom_sf(aes(fill=CLU))+
+cols<-c("G1"="#ca0020","G2"="#f4a582","G3"="#92c5de","G4"="#0571b0")
+
+map.dtw.h04<-ggplot()+
+  geom_sf(data=ltla,aes(fill=CLU),linewidth=0.2,colour="gray60")+
   #scale_fill_brewer(palette = "OrRd",labels=c("Not Sig.","Hot spot","Cold spot"))
-  #scale_fill_manual(values = gi.map,name="Gi* values")+
+  scale_fill_manual(values=cols,drop=FALSE)+
   theme_bw()+
-  theme(legend.position = "bottom",plot.title = element_text(size =30, face = "bold"),
-        legend.title=element_text(size=25), 
-        legend.text=element_text(size=25))+
-  annotation_scale(location = "br", height = unit(0.5, "cm")) +
-  annotation_north_arrow(location = "tl", 
+  theme(text=element_text(size=rel(6), family="notosanskr",),
+        plot.title = element_text(size =rel(5), family="notosanskr",
+                                  face = "bold"),
+        legend.title=element_text(size =rel(5), family="notosanskr",
+                                  face = "bold"), 
+        legend.text=element_text(size =rel(5), family="notosanskr",
+                                 face = "bold"),
+        legend.position= "bottom")+
+        annotation_scale(location = "br", height = unit(0.5, "cm")) +
+        annotation_north_arrow(location = "tl", 
                          style = north_arrow_nautical, 
                          height = unit(2, "cm"),
                          width = unit(2, "cm"))+
-  geom_sf(data=eng.la,fill=NA,color="black",size=1,show.legend=FALSE)+
+  labs(fill="Cluster",x=NULL,y=NULL)+
+  geom_sf(data=eng.la,fill=NA,color="black",linewidth=0.3,
+          show.legend=FALSE)+
   geom_label_repel(data=la.coords, aes(X, Y, label = NAME),
-                   colour = "Black",size=10,family="Raleway",fontface="bold",
-                   box.padding = unit(1, "lines"),
-                   point.padding = unit(1, "lines"),
-                   segment.color = 'grey50')
-ggsave("map.dtw.h04.per.png", width=400, height=400, units = "mm", dpi = 300, bg = "white")
+                   colour = "Black",size=rel(15),
+                   family="notosanskr",
+                   fontface="bold",
+                   alpha=.7,
+                   segment.color = 'gray50')
+ggsave("map.dtw.h04.per.png", width=200, height=200, 
+       units = "mm", dpi = 300, bg = "white")
 
 graph.data<-left_join(tbl.mob.per,h04.per,by="LTLA19CD")
 graph.data$Cluster<-as.factor(graph.data$CLU)
 #graph.data<-subset(graph.data,date>="2020-03-23" & date<"2020-05-11")
 
-plot.trend.Jul<- ggplot(data=graph.data,aes(x=date, y=ts(m.per.14),fill=Cluster))+
+plot.trend.Jul<- ggplot(data=graph.data,aes(x=date, y=ts(m.per.07),
+                                            fill=CLU))+
   theme_bw()+
+  theme(text=element_text(size=rel(6.5), family="notosanskr"),
+        plot.title = element_text(size =rel(5), family="notosanskr",
+                                  face = "bold"),
+        legend.title=element_text(size =rel(5), family="notosanskr",
+                                  face = "bold"), 
+        legend.text=element_text(size =rel(5), family="notosanskr",
+                                 face = "bold"),
+        strip.text.x = element_text(size =rel(10), family="notosanskr",
+        face = "bold"),
+        legend.position= "bottom")+
   #geom_line(color="grey20",size=0.02,alpha=0.4)+
-  geom_boxplot(aes(x=date, y=m.per.14,group=date),colour="gray50",outlier.shape=0.2)+
-  labs(x="Day",y="14-day rolling average of mobility reduction compared to the baseline (%)",face="bold")+
-  scale_x_date(labels=date_format("%d %b"))+
-  scale_y_continuous(limits=c(-100, 0))+
-  geom_smooth(method ="loess",span=0.2,fill="gray80")+
+  geom_boxplot(aes(x=date, y=m.per.07,group=date),
+               colour="gray50",alpha=.8,outlier.shape=0.1,linewidth=0.1)+
+  scale_fill_manual(values=cols,drop=FALSE)+
+  labs(x="Day",y="7-day rolling average of mobility reduction compared to the baseline (%)",
+       fill="Cluster", face="bold",size=rel(15), family="notosanskr",)+
+  scale_x_date(labels=date_format("%d %b"),breaks="2 months")+
+  scale_y_continuous(limits=c(-100, 25))+
+  geom_smooth(method ="loess",span=0.2,colour="black",linewidth=0.5,
+              se=TRUE)+
   geom_vline(xintercept=graph.data$date[graph.data$date=="2020-03-03"],linetype=2, colour="black")+
   geom_vline(xintercept=graph.data$date[graph.data$date=="2020-03-23"],linetype=2, colour="red")+
   geom_vline(xintercept=graph.data$date[graph.data$date=="2020-05-11"],linetype=2, colour="red")+
   facet_grid(~CLU)
-ggsave("plot.temp.trend.m.14.per.Jul.png", width=300, height=200, units = "mm", dpi = 300, bg = "white")
+ggsave("plot.temp.trend.m.7.per.Jul.png", width=150, height=100, 
+       scale=2,units = "mm", dpi = 300, bg = "white")
 
 require(gridExtra)
 plot.whole<-grid.arrange(plot.whole.med.per.jun,plot.trend.Jun,ncol=2)
